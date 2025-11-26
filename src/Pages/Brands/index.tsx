15802620c86055aea38173804fb7b2df9c22674d
Abdelrahman-Moharram <abd0m07arram@yahoo.com>
2025-11-27 00:15:16 +0200

    add project files

--- src/Pages/Brands/index.tsx
+++ src/Pages/Brands/index.tsx
@@ -6,9 +6,7 @@ import { useGetBrandsListQuery } from '../../redux/api/brandsApi'
 const Brands = () => {
 
   const {data, isLoading}        = useGetBrandsListQuery({page:1, size:10})
-  // const [searchParams]           = useSearchParams()
-  // const size                     = to_int_or_default(searchParams.get("size")) 
-  // const page                     = to_int_or_default(searchParams.get("page")) 
+
   
   return (
     <div className='bg-white p-5 my-8 default-shadow-md'>
--- src/Pages/Settings/_Components/CardsListWithPagination.tsx
+++ src/Pages/Settings/_Components/CardsListWithPagination.tsx
@@ -19,65 +19,48 @@ const handleImageSkeleton = () =>{
 }
 
 interface Props{
-    isLoading: boolean,
-    data: any[],
-    editAction?:(row:any)=>void,
-    deleteAction?:(row:any)=>void,
-    total_pages:number,
-    emptyMessage?:string
+    isLoading       : boolean,
+    data            : any[],
+    editAction?     : (row:any)=>void,
+    deleteAction?   : (row:any)=>void,
+    total_pages     : number,
+    emptyMessage?   : string
 }
 const CardsListWithPagination = ({data, deleteAction, editAction, isLoading, total_pages, emptyMessage}:Props) => {
     
   return (
     <div>
       {
-            !isLoading?
-                data?.length?
-                    // Cards
-
-                    <div className="grid lg:grid-cols-4 md:lg:grid-cols-3 sm:lg:grid-cols-2 xs:lg:grid-cols-1 gap-3">
-                        {
-                            data.map((item:{id:string, name:string})=>(
-                                <FnBasicCard
-                                    key={item?.id}
-                                    keyName={item?.name}
-                                >
-                                    <EditDeleteButtons
-                                        editAction={editAction}
-                                        deleteAction={deleteAction}
-                                        item={item}
-                                    />
-                                </FnBasicCard>
-                            ))
-                        }
-                        
-                    </div>
-
-                    // Table
-
-                    // <div className="w-[100%] mx-auto">
-                    //     <DataTable
-                    //         data={data}
-                    //         emptyLinkHref=''
-                    //         emptyText=''
-                    //         fnKeys={['id']}
-                    //         isLoading={isLoading}
-                    //         isOptions
-                    //         options={customOptions?customOptions:options}
-                    //         optionsHeader=''
-                    //         showCounter
-                    //     />
-                    // </div>
-
-                :
-                    <EmptyData
-                        height='100px'
-                        message={emptyMessage || 'No Data'}
-                    />
-            :
+            
+        isLoading?
+            <div className="grid lg:grid-cols-4 md:lg:grid-cols-3 sm:lg:grid-cols-2 xs:lg:grid-cols-1 gap-3">
+                {handleImageSkeleton()}
+            </div>
+        :
+            data?.length?
                 <div className="grid lg:grid-cols-4 md:lg:grid-cols-3 sm:lg:grid-cols-2 xs:lg:grid-cols-1 gap-3">
-                    {handleImageSkeleton()}
+                    {
+                        data.map((item:{id:string, name:string})=>(
+                            <FnBasicCard
+                                key={item?.id}
+                                keyName={item?.name}
+                            >
+                                <EditDeleteButtons
+                                    editAction={editAction}
+                                    deleteAction={deleteAction}
+                                    item={item}
+                                />
+                            </FnBasicCard>
+                        ))
+                    }
                 </div>
+
+            :
+                <EmptyData
+                    height='100px'
+                    message={emptyMessage || 'No Data'}
+                />
+                
         }
     
         <div className='flex justify-center my-10 font-extrabold'>
